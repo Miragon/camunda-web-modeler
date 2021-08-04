913c65a6d32b9c243449485b4e8a188d3914113e
Alexander Praschek <alexander.praschek@flowsquad.io>
2021-08-04 09:25:10 +0200

    Add README, additional exports from index

---
+++ src/bpmnio/index.ts
@@ -0,0 +1,31 @@
+export {
+    isBpmnIoEvent
+} from "./bpmnio/BpmnIoEvents";
+
+export {
+    isContentSavedEvent
+} from "./modeler/ContentSavedEvent";
+
+export type {
+    ContentSavedReason
+} from "./modeler/ContentSavedEvent";
+
+export {
+    isDmnViewsChangedEvent
+} from "./modeler/DmnViewsChangedEvent";
+
+export {
+    isNotificationEvent
+} from "./modeler/NotificationEvent";
+
+export {
+    isPropertiesPanelResizedEvent
+} from "./modeler/PropertiesPanelResizedEvent";
+
+export {
+    isUIUpdateRequiredEvent
+} from "./modeler/UIUpdateRequiredEvent";
+
+export type {
+    Event
+} from "./Events";
